53d447b1f68d1c066c420fcf71569d445cad1776
Kerim Demir <kerim.demir2002@hotmail.com>
2023-08-09 21:32:11 +0300

    init

---
+++ src/main/java/com/gettogetherbackend/GetTogetherBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gettogetherbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetTogetherBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetTogetherBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gettogetherbackend/GetTogetherBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gettogetherbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetTogetherBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
